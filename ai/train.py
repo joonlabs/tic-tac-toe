3d3baae1a27f0b4c094f8ada0bc0b0ea4c62dbd5
Julius Türich <info@joonlabs.com>
2023-04-07 13:56:25 +0200

    change network structure save model of tensorflowjs

--- ai/train.py
+++ ai/train.py
@@ -2,6 +2,8 @@ import tensorflow as tf
 import numpy as np
 import json
 
+import tensorflowjs as tfjs
+
 # load dataset.json
 with open("dataset.json", "r") as f:
     dataset = json.loads(f.read())
@@ -25,18 +27,23 @@ validation_set = training_data[split:]
 training_set = np.array(training_set)
 validation_set = np.array(validation_set)
 
+print(training_set[:, 0].shape)
+
 # create model
 model = tf.keras.models.Sequential([
-    tf.keras.layers.Flatten(input_shape=(9,)),
+    tf.keras.layers.InputLayer(input_shape=(9,)),
     tf.keras.layers.Dense(128, activation=tf.nn.relu),
-    tf.keras.layers.Dense(9, activation=tf.nn.tanh)
+    tf.keras.layers.Dense(64, activation=tf.nn.relu),
+    tf.keras.layers.Dense(9, activation=tf.nn.softmax)
 ])
 
 # compile model
-model.compile(optimizer="adam", loss="mean_squared_error", metrics=["accuracy"])
+model.compile(optimizer="adam", loss=tf.losses.binary_crossentropy, metrics=["accuracy"])
 
 # train model
-model.fit(training_set[:, 0], training_set[:, 1], epochs=10, validation_data=(validation_set[:, 0], validation_set[:, 1]))
+model.fit(training_set[:, 0], training_set[:, 1], epochs=20, validation_data=(validation_set[:, 0], validation_set[:, 1]))
 
 # save model
-model.save("model.h5")
+model.save("models/model.h5")
+tfjs.converters.save_keras_model(model, "models/js")
+
