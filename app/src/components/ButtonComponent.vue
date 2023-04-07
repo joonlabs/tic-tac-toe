ef8379e8d98e040ff8c1d49c92ae4596c67befc4
Julius Türich <info@joonlabs.com>
2023-04-07 13:58:44 +0200

    add `ButtonComponent`

---
+++ app/src/components/ButtonComponent.vue
@@ -0,0 +1,30 @@
+<template>
+  <button>
+    <slot/>
+  </button>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+
+export default defineComponent({
+})
+</script>
+
+<style lang="scss" scoped>
+button {
+  outline: none;
+  background: var(--color-accent);
+  border-radius: var(--border-radius);
+  border: none;
+  padding: .85rem 1.9rem;
+  font-size: .95rem;
+  color: white;
+  font-weight: bold;
+  cursor: pointer;
+
+  &:active {
+    transform: scale(.975);
+  }
+}
+</style>
